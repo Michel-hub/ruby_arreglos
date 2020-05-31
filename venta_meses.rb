8bf6d66045ad551bd343cfb3677d1ba88cd53e32
Michel-hub <mc.ubilla93@gmail.com>
2020-05-31 18:08:57 -0400

    nuevohash

---
+++ venta_meses.rb
@@ -0,0 +1,26 @@
+ventas = {
+    Octubre: 65000,
+    Noviembre: 68000,
+    Diciembre: 72000
+}
+
+ventas.each do |k,v|
+   ventas[k] *= 1.1
+
+end
+
+puts ventas 
+
+ventas1 = {
+    Octubre: 65000,
+    Noviembre: 68000,
+    Diciembre: 72000
+}
+
+nuevo_ventas = {}
+
+ventas1.each do |k,v|
+    nuevo_ventas[k] = v * 0.8
+ end
+ 
+puts nuevo_ventas 
